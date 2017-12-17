b713b59e6129b08a5d0b2b543ba9e38df701f2c8
ammediagroup <andrey@ammediagroup.org>
2017-12-17 21:31:33 +0300

    00

---
+++ WebApplication1/Models/GuestResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Please enter your name")]
+        public string Name{get; set;}
+        [Required(ErrorMessage = "Please enter your email")]
+        [RegularExpression(@".+\@.+\..+",ErrorMessage = "Please enter your name")]
+        public string Email{ get; set; }
+        [Required(ErrorMessage = "phone")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "attend")]
+        public bool? WillAttend { get; set; }
+
+    }
+}
